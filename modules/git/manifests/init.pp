0027a03ae631b85e05bd49c182203b482940cef7
meza <reg@meza.hu>
2011-03-20 16:01:19 +0100

    pull

--- modules/git/manifests/init.pp
+++ modules/git/manifests/init.pp
@@ -12,8 +12,17 @@ class git {
     define clone ( $repository, $targetDir ) {
 	exec {
 	    "git clone":
+	    unless  => "/usr/bin/test -d $targetDir",
 	    command => "/usr/bin/git clone -q $repository $targetDir",
 	    require => Package["git-core"],
+	}
+	
+	exec {
+	    "git pull":
+		onlyif  => "/usr/bin/test -d $targetDir",
+		cwd     => $targetDir,
+		command => "/usr/bin/git pull",
+		require => Package["git-core"]
 	}
     }
 }
